a762f73c6f4d121bdc1be057a723a69c64f963d7
Oscar <oscar@gmail.com>
2019-09-02 11:05:23 -0400

    worflow de webpack

---
+++ README.md
@@ -0,0 +1,5 @@
+# WorkFlow de webpack
+worflow de webpack para transpilar css, js, motor de plantillas(.handlebars) files and images.
+del curso practico de [FaztWeb](https://www.youtube.com/watch?v=7e5apiL6tVQ)
+
+nota: no quiere funcionar el comprimido de imagenes :c 
---
+++ webpack/webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        path: path.resolve(__dirname,'../build'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            autoprefixer: {
+                                browser: ["last 2 versions"]
+                            },
+                            plugins: () => [
+                                autoprefixer
+                            ]
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test:/\.handlebars$/,
+                loader: 'handlebars-loader'
+            },
+            {
+                test: /\.(jpg|png|gif|jpeg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name:'[name].[ext]',
+                            outputPath: 'static/',
+                            useRelativePath: true
+                        }
+                    },
+                    /*{
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }*/
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.handlebars',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename:'css/[name].css',
+            chunkFilename: "[id].css"
+        })
+    ]
+};
